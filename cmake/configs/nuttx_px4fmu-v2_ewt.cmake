d166c0b09989cad3b9a3ca5189989aa146e9dff5
yl <165577564@qq.com>
2018-09-13 00:28:33 -0700

    update submodule commit id

--- cmake/configs/nuttx_px4fmu-v2_ewt.cmake
+++ cmake/configs/nuttx_px4fmu-v2_ewt.cmake
@@ -7,38 +7,47 @@ set(config_module_list
 	# Board support modules
 	#
 	#drivers/barometer
-	drivers/differential_pressure
+	#drivers/differential_pressure
+	#drivers/distance_sensor
 	#drivers/magnetometer
 	#drivers/telemetry
 
-	#drivers/imu/adis16448
-	drivers/airspeed
+	#drivers/barometer/bmp280
 	drivers/barometer/ms5611
 	#drivers/blinkm
-	#drivers/imu/bmi160
-	#drivers/barometer/bmp280
-	drivers/boards
 	#drivers/bst
 	drivers/camera_trigger
-	drivers/device
+	#drivers/differential_pressure/ets
+	drivers/differential_pressure/ms4525
+	drivers/differential_pressure/ms5525
+	#drivers/differential_pressure/sdp3x
+	#drivers/distance_sensor/ll40ls
+	#drivers/distance_sensor/mb12xx
+	#drivers/distance_sensor/sf0x
+	#drivers/distance_sensor/sf1xx
+	#drivers/distance_sensor/srf02
+	#drivers/distance_sensor/teraranger
+	#drivers/distance_sensor/tfmini
+	#drivers/distance_sensor/ulanding
 	#drivers/frsky_telemetry
 	drivers/gps
 	#drivers/hott
+	#drivers/imu/adis16448
+	#drivers/imu/bmi160
+	drivers/imu/l3gd20
+	drivers/imu/lsm303d
+	drivers/imu/mpu6000
+	drivers/imu/mpu9250
 	#drivers/iridiumsbd
 	#drivers/irlock
-	drivers/imu/l3gd20
-	drivers/led
-	drivers/imu/lsm303d
 	drivers/magnetometer/hmc5883
 	drivers/magnetometer/lis3mdl
 	#drivers/mb12xx
 	#drivers/mkblctrl
-	drivers/imu/mpu6000
-	#drivers/imu/mpu9250
 	#drivers/oreoled
 	#drivers/protocol_splitter
 	drivers/pwm_input
-	drivers/pwm_out_sim
+	#drivers/pwm_out_sim
 	drivers/px4flow
 	drivers/px4fmu
 	drivers/px4io
@@ -48,17 +57,6 @@ set(config_module_list
 	drivers/stm32/tone_alarm
 	#drivers/tap_esc
 	drivers/vmount
-
-	# distance sensors
-	#drivers/distance_sensor/ll40ls
-	#drivers/distance_sensor/mb12xx
-	#drivers/distance_sensor/sf0x
-	#drivers/distance_sensor/sf1xx
-	#drivers/distance_sensor/srf02
-	#drivers/distance_sensor/srf02_i2c
-	#drivers/distance_sensor/teraranger
-	#drivers/distance_sensor/tfmini
-	#drivers/distance_sensor/ulanding
 	modules/sensors
 
 	#
@@ -94,7 +92,6 @@ set(config_module_list
 	#lib/rc/rc_tests
 	#modules/commander/commander_tests
 	#modules/mavlink/mavlink_tests
-	#modules/mc_pos_control/mc_pos_control_tests
 	#modules/uORB/uORB_tests
 	#systemcmds/tests
 
@@ -126,8 +123,8 @@ set(config_module_list
 	#
 	modules/fw_att_control
 	modules/fw_pos_control_l1
-#	modules/gnd_att_control
-#	modules/gnd_pos_control
+	#modules/gnd_att_control
+	#modules/gnd_pos_control
 	modules/mc_att_control
 	modules/mc_pos_control
 	modules/vtol_att_control
@@ -136,32 +133,11 @@ set(config_module_list
 	# Logging
 	#
 	modules/logger
-	#modules/sdlog2
 
 	#
 	# Library modules
 	#
 	modules/dataman
-	modules/systemlib
-	modules/systemlib/param
-	modules/uORB
-
-	#
-	# Libraries
-	#
-	lib/controllib
-	lib/conversion
-	lib/DriverFramework/framework
-	lib/ecl
-	lib/geo
-	lib/rc
-	lib/geo_lookup
-	lib/led
-	lib/mathlib
-	lib/mixer
-	#lib/terrain_estimation
-	lib/tunes
-	lib/version
 
 	#
 	# OBC challenge
@@ -197,3 +173,5 @@ set(config_module_list
 	# Hardware test
 	#examples/hwtest
 )
+
+set(flight_tasks_to_remove Orbit)
